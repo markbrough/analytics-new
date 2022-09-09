74472a9da9d25ea874116722094a5b7eef3ff969
markbrough <markbrough@users.noreply.github.com>
2022-09-09 21:07:01 +0000

    deploy: b3a99b42fbe672bf2f5605d8d3cc47e7c48a1a91

---
+++ _nuxt/199a160.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{476:function(n,e,l){"use strict";l.r(e);var t={name:"IndexPage"},o=l(81),component=Object(o.a)(t,(function(){return(0,this._self._c)("div",[this._v("Hello")])}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1662757620/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fanalytics-new\u002F_nuxt\u002Fstatic\u002F1662757620",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fanalytics-new\u002F",assetsPath:"\u002Fanalytics-new\u002F_nuxt\u002F",cdnURL:a}}}}(null));
